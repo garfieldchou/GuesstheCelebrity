ff3ee60f387382c453019da0a28bd76f9b75b2e7
garfield_chou <garfield_chou@asus.com>
2017-01-04 10:43:47 +0800

    Set single onClick function for buttons and assign tag for each

--- app/src/main/java/com/garfield_chou/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/garfield_chou/guessthecelebrity/MainActivity.java
@@ -2,8 +2,16 @@ package com.garfield_chou.guessthecelebrity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void chooseName (View view) {
+
+        Log.i("button tapped", view.getTag().toString());
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
